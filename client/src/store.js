dcc56edb70f84fc4f1ea9fa3e719faecaa306f78
jjaijg <jjaiganeshjg95@gmail.com>
2020-01-20 14:06:23 +0530

    Removing redux devtool, because of the compatability issue in mobile site

--- client/src/store.js
+++ client/src/store.js
@@ -9,10 +9,8 @@ const middleWare = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleWare))
 );
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 export default store;
