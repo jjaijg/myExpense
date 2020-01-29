9c1a4ee1b39af9f2f64bc43b91285bcabb2e6edb
root <root@ip-10-0-4-123.ap-southeast-1.compute.internal>
2020-01-29 06:17:42 +0000

    update-1 : Transaction model

--- models/transactions.js
+++ models/transactions.js
@@ -5,9 +5,10 @@ const ObjectId = Schema.ObjectId;
 
 const TransactionSchema = new Schema({
   doneBy: ObjectId,
-  doneFor: { type: String, required: true },
-  expense: { type: Number, required: true },
-  doneAt: { type: Date, default: Date.now }
+  doneFor: { type: String, required: 'Please Enter Purpose' },
+  expense: { type: Number, required: 'Please enter Expense' },
+  doneAt: { type: Date, default: Date.now },
+  type: {type: String, default: 'd'}
 });
 
 module.exports = Transaction = mongoose.model("transaction", TransactionSchema);
