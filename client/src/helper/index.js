5ba15917248b95556bba2b34155e77ec569c7377
jjaijg <jjaiganeshjg95@gmail.com>
2020-01-29 06:55:48 +0530

    added color generator helper function

--- client/src/helper/index.js
+++ client/src/helper/index.js
@@ -10,3 +10,23 @@ export function compare(a, b) {
   }
   return comparison;
 }
+
+export const colorGenerator = n => {
+  // storing all letter and digit combinations
+  // for html color code
+  let colors = [];
+  let letters = "0123456789ABCDEF";
+
+  // html color code starts with #
+  let color = "#";
+
+  // generating 6 times as HTML color code consist
+  // of 6 letter or digits
+  for (let _ = 0; _ < n; _++) {
+    for (let i = 0; i < 6; i++)
+      color += letters[Math.floor(Math.random() * 16)];
+    colors.push(color);
+    color = "#";
+  }
+  return colors;
+};
